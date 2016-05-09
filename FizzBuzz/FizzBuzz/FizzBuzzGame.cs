7fa13069fe410c37757c206d41b91c3002fd2d26
hkoundi <hkoundi@gmail.com>
2016-05-09 08:00:52 +0100

    fizzbuzz

---
+++ FizzBuzz/FizzBuzz/FizzBuzzGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+	public class FizzBuzzGame
+	{
+		public static object printValue(int givenValue ){
+			var isMultipleOfThree = (givenValue % 3 == 0);
+			var isMultipleOfFive = (givenValue % 5 == 0);
+			if (isMultipleOfThree) {
+				return "Fizz";
+			} else if (isMultipleOfFive) {
+				return "Buzz";
+			}
+			return givenValue;	
+		}
+	}
+}
+
