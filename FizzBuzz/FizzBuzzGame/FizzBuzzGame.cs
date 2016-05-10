31845c27c32b8dae615c01ae181559ac858814b1
hkoundi <hkoundi@gmail.com>
2016-05-10 08:40:05 +0100

    feedback1

---
+++ FizzBuzz/FizzBuzzGame/FizzBuzzGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+	public class FizzBuzzGame
+	{
+		public FizzBuzzGame ()
+		{
+		}
+
+		public string ReturnValue(int value)
+		{
+			var isFizz = value % 3 == 0;
+			var isBuzz = value % 5 == 0;
+			if (isFizz && isBuzz) 
+			{
+				return "FizzBuzz";
+			}
+			else if (isFizz) 
+			{
+				return "Fizz";
+			}
+			else if(isBuzz)
+			{
+				return "Buzz";
+			}
+			return value.ToString();
+		}
+	}
+}
+
---
+++ FizzBuzz/FizzBuzzTests/FizzBuzzGameTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using FizzBuzz;
+
+namespace FizzBuzzTests
+{
+	[TestFixture]
+	public class FizzBuzzGameTests
+	{
+
+		FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+
+		[TestCase]
+		public void give_1_return_1()
+		{	
+			Assert.AreEqual (1.ToString(), fizzBuzzGame.ReturnValue(1));
+		}
+
+		[TestCase]
+		public void give_2_return_2()
+		{
+			Assert.AreEqual (2.ToString(), fizzBuzzGame.ReturnValue(2));
+		}
+
+		[TestCase]
+		public void give_3_return_Fizz()
+		{
+			Assert.AreEqual ("Fizz", fizzBuzzGame.ReturnValue(3));
+		}
+
+		[TestCase]
+		public void give_5_return_Buzz()
+		{
+			Assert.AreEqual ("Buzz", fizzBuzzGame.ReturnValue(5));
+		}
+
+		[TestCase]
+		public void give_15_return_FizzBuzz()
+		{
+			Assert.AreEqual ("FizzBuzz", fizzBuzzGame.ReturnValue(15));
+		}
+
+		[TestCase]
+		public void give_150_return_FizzBuzz()
+		{
+			Assert.AreEqual ("FizzBuzz", fizzBuzzGame.ReturnValue(150));
+		}
+	}
+}
+
