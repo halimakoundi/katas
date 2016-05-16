dc1e6ba896b58c0de791ee3ea6a3a70fdfb667d3
Halima KOUNDI <hkoundi@gmail.com>
2016-05-16 14:43:31 +0100

    extracting method is divisible by

--- LeapYear/LeapYearKata/LeapYearKata/LeapYear.cs
+++ LeapYear/LeapYearKata/LeapYearKata/LeapYear.cs
@@ -5,14 +5,19 @@ namespace LeapYearKata
     {
         public bool IsLeapYear(int year)
         {
-            var isDivisbleBy100 = year % 100 == 0;
-            var isDivisibleBy400 = year % 400 == 0;
-            var isDivisibleBy4 = year % 4 == 0;
+            bool isDivisbleBy100 = IsDivisibleBy(year, 100);
+            var isDivisibleBy400 = IsDivisibleBy(year, 400);
+            var isDivisibleBy4 = IsDivisibleBy(year, 4);
             if (isDivisibleBy4 && !isDivisbleBy100 || isDivisibleBy400)
             {
                 return true;
             }
             return false;
+        }
+
+        private static bool IsDivisibleBy(int year, int denominator)
+        {
+            return year % denominator == 0;
         }
     }
 }
