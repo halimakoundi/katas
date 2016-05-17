532281ccf0c09bf15352ff35b8d9dc84c7a7187d
Halima KOUNDI <hkoundi@gmail.com>
2016-05-17 14:52:13 +0100

    refactoring previously extracted method into a prototype function is divisble by

--- Javascript/LeapYear/leapyear.js
+++ Javascript/LeapYear/leapyear.js
@@ -1,22 +1,13 @@
 exports.isLeapYear = function (year)
 {
-    if (  (isDivisibleBy4(year) && !isDivisibleBy100(year))
-        || isDivisibleBy400(year))
+    if ((year.isDivisibleBy(4) && !year.isDivisibleBy(100))
+        || year.isDivisibleBy(400))
     {
         return true;
     }
     return false;
 }
 
-function isDivisibleBy4(year)
-{
-    return year % 4 == 0;
-}
-function isDivisibleBy400(year)
-{
-    return year % 400 == 0;
-}
-function isDivisibleBy100(year)
-{
-    return year % 100 == 0;
+Number.prototype.isDivisibleBy = function (denominator) {
+    return this % denominator == 0;
 }
