40656c11ac3e808f5183f2b1709f9168b2215b0f
Halima KOUNDI <hkoundi@gmail.com>
2016-05-17 15:19:52 +0100

    merging test methods usng testcase attributes

--- FizzBuzz/FizzBuzzGameTests/FizzBuzzGameTests/FizzBuzzTests.cs
+++ FizzBuzz/FizzBuzzGameTests/FizzBuzzGameTests/FizzBuzzTests.cs
@@ -6,20 +6,13 @@ namespace FizzBuzzGameTests
     [TestFixture]
     public class FizzBuzzTests
     {
-        [Test]
-        public void GiveOneReturnsOne()
+        [TestCase(1,1)]
+        [TestCase(2,2)]
+        public void GiveValueReturnsValue(int value, int expected)
         {
             var fizzbuzz = new FizzBuzz();
-            var returnedValue = fizzbuzz.ReturnValue(1);
-            Assert.That(returnedValue, Is.EqualTo(1));
-        }
-
-        [Test]
-        public void GiveTwoReturnsTwo()
-        {
-            var fizzbuzz = new FizzBuzz();
-            var returnedValue = fizzbuzz.ReturnValue(2);
-            Assert.That(returnedValue, Is.EqualTo(2));
+            var returnedValue = fizzbuzz.ReturnValue(value);
+            Assert.That(returnedValue, Is.EqualTo(expected));
         }
     }
 }
