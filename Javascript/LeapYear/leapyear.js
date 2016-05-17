d1e800103eeadeb4d98971e6f37c673789489804
Halima KOUNDI <hkoundi@gmail.com>
2016-05-17 14:54:35 +0100

    streamline of the function

--- Javascript/LeapYear/leapyear.js
+++ Javascript/LeapYear/leapyear.js
@@ -1,11 +1,7 @@
-exports.isLeapYear = function (year)
-{
-    if ((year.isDivisibleBy(4) && !year.isDivisibleBy(100))
-        || year.isDivisibleBy(400))
-    {
-        return true;
-    }
-    return false;
+exports.isLeapYear = function (year) {
+
+    return    (year.isDivisibleBy(4) && !year.isDivisibleBy(100))
+            || year.isDivisibleBy(400);
 }
 
 Number.prototype.isDivisibleBy = function (denominator) {
