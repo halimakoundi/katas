e6529c067b57d93c402841abee9ad92c5ce0c385
Halima KOUNDI <hkoundi@gmail.com>
2016-05-18 19:08:33 +0100

    Zero generates an ampty sequence

---
+++ Fibonacci/FibonacciKata/FibonacciKata.Src/Fibonacci.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace FibonacciKata.Src
+{
+    public class Fibonacci
+    {
+        public List<int> Generate(int sequenceLength)
+        {
+            return new List<int>();
+        }
+    }
+}
---
+++ Fibonacci/FibonacciKata/FibonacciKata.Tests/FibonacciShould.cs
@@ -0,0 +1,27 @@
+﻿using FibonacciKata.Src;
+using NUnit.Framework;
+
+/*
+ *The Fibonacci sequence 
+ * (named after the Italian mathematician Leonardo Fibonacci) 
+ * is a sequence of numbers where each number 
+ * is the sum of the previous two numbers.
+ *
+ **/
+
+namespace FibonacciKata.Tests
+{
+    [TestFixture]
+    public class FibonacciShould    
+    {
+        [Test]
+        public void return_an_empty_sequence_given_length_zero()
+        {
+            var fibonacci = new Fibonacci();
+
+            var sequence = fibonacci.Generate(0);
+
+            Assert.That(sequence, Is.Empty );
+        }
+    }
+}
