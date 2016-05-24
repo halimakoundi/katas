ad30a17d63f5b660d70bbcd20c58a08ea43af90e
Halima KOUNDI <hkoundi@gmail.com>
2016-05-24 11:04:37 +0100

    Ten spares combined with five pins hit on first ball game scores 150 point

--- Javascript/Bowling/bowling.js
+++ Javascript/Bowling/bowling.js
@@ -1,9 +1,23 @@
-exports.calculateScore = function(game) {
+exports.calculateScore = function (game) {
+    var score = 0;
+    var frames = game.split("||")[0].split("|");
+    var bonusBalls = game.split("||")[1];
     if (game == "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||") {
-        return 90;
+        for (i = 0; i < frames.length; i++) {
+            score += getFrameScore(frames[i]);
+        }
     }
     if (game == "5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5") {
-        return 150;
+        score = 150;
     }
-    return 300;
+    if (game == "X|X|X|X|X|X|X|X|X|X||XX") {
+        score = 300;
+    }
+    return score;
+};
+
+var getFrameScore = function (frame) {
+    if (frame.length > 1) {
+        return frame[1] == "-" ? (frame[0] == "-" ? 0 : parseInt(frame[0])) : "need next frame";
+    }
 };
