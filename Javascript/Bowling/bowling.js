72874c5457c857ef576cc850f4dd0438a5466dcc
Halima KOUNDI <hkoundi@gmail.com>
2016-05-24 12:18:01 +0100

    Extracting get next ball score method

--- Javascript/Bowling/bowling.js
+++ Javascript/Bowling/bowling.js
@@ -2,14 +2,20 @@ exports.calculateScore = function (game) {
     var score = 0;
     var frames = game.split("||")[0].split("|");
     var bonusBalls = game.split("||")[1];
-
     if (game == "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||") {
         for (i = 0; i < frames.length; i++) {
-            score += getFrameScore(frames[i]);
+            var frameScore = getFrameScore(frames[i]);
+            score += frameScore;
         }
     }
     if (game == "5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5") {
-        score = 150;
+        for (i = 0; i < frames.length; i++) {
+            var frameScore = getFrameScore(frames[i]);
+            if (frameScore == "/") {
+                score += 10 + ((i < frames.length - 1) ? getNextBallScore(frames[i + 1])
+                        : parseInt(bonusBalls[0]));
+            } else { score += parseInt(frameScore); }
+        }
     }
     if (game == "X|X|X|X|X|X|X|X|X|X||XX") {
         score = 300;
@@ -18,7 +24,16 @@ exports.calculateScore = function (game) {
 };
 
 var getFrameScore = function (frame) {
-    if (frame.length > 1) {
-        return frame[1] == "-" ? (frame[0] == "-" ? 0 : parseInt(frame[0])) : "need next frame";
+    if (frame == "X") {
+        return "this is a strike we need next two balls";
     }
+    if (frame.length > 1) {
+        return frame[1] == "-"
+            ? (frame[0] == "-" ? 0 : parseInt(frame[0]))
+            : "/";
+    }
+};
+
+var getNextBallScore = function(frame) {
+    return parseInt(frame[0]);
 };
