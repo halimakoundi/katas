bbeb6a2b5c70bd40b945d9a37f4f6a1d6440dea2
Halima KOUNDI <hkoundi@gmail.com>
2016-05-24 12:42:38 +0100

    Merging cases

--- Javascript/Bowling/bowling.js
+++ Javascript/Bowling/bowling.js
@@ -2,30 +2,24 @@ exports.calculateScore = function (game) {
     var score = 0;
     var frames = game.split("||")[0].split("|");
     var bonusBalls = game.split("||")[1];
-    if (game == "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||") {
-        for (i = 0; i < frames.length; i++) {
-            var frameScore = getFrameScore(frames[i]);
-            score += frameScore;
-        }
-    }
-    if (game == "5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5") {
-        for (i = 0; i < frames.length; i++) {
-            var frameScore = getFrameScore(frames[i]);
-            if (frameScore == "/") {
-                score += 10 + ((i < frames.length - 1) ? getNextBallScore(frames[i + 1])
-                        : parseInt(bonusBalls[0]));
-            } else { score += parseInt(frameScore); }
-        }
-    }
-    if (game == "X|X|X|X|X|X|X|X|X|X||XX") {
-        score = 300;
+    for (i = 0; i < frames.length; i++) {
+        var frameScore = getFrameScore(frames[i]);
+        if (frameScore == "/") {
+            score += 10 + ((i < frames.length - 1) ? getNextBallScore(frames[i + 1])
+                    : parseInt(bonusBalls[0]));
+        } else if (frameScore == "X") {
+            score += 10 + ((i < frames.length - 2) ? (getNextBallScore(frames[i + 1]) + getNextBallScore(frames[i + 2]))
+                : (((i < frames.length - 1) ? (getNextBallScore(frames[i + 1]) + getNextBallScore(bonusBalls[0]))
+                    : (getNextBallScore(bonusBalls[0]) + getNextBallScore(bonusBalls[1])))
+                    ));
+        } else { score += parseInt(frameScore); }
     }
     return score;
 };
 
 var getFrameScore = function (frame) {
     if (frame == "X") {
-        return "this is a strike we need next two balls";
+        return "X";
     }
     if (frame.length > 1) {
         return frame[1] == "-"
@@ -34,6 +28,7 @@ var getFrameScore = function (frame) {
     }
 };
 
-var getNextBallScore = function(frame) {
+var getNextBallScore = function (frame) {
+    if (frame[0] == "X") return 10;
     return parseInt(frame[0]);
 };
