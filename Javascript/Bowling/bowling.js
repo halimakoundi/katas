a5c40ee6688f616f3a7cbd1f70261e3d16c730bc
Halima KOUNDI <hkoundi@gmail.com>
2016-05-24 10:19:14 +0100

    Ten nine pins hit game scores 90 points

--- Javascript/Bowling/bowling.js
+++ Javascript/Bowling/bowling.js
@@ -1,3 +1,6 @@
-exports.calculateScore = function() {
+exports.calculateScore = function(game) {
+    if (game == "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||") {
+        return 90;
+    }
     return 300;
 };
--- Javascript/Bowling/spec/bowling-spec.js
+++ Javascript/Bowling/spec/bowling-spec.js
@@ -1,8 +1,19 @@
 var bowling = require("../bowling.js");
 
-describe("Ten strikes game scores 300 points", function() {
-    it("Should return 300 as the score for X|X|X|X|X|X|X|X|X|X||XX game", function () {
-        var score = bowling.calculateScore("X|X|X|X|X|X|X|X|X|X||XX");
-        expect(score).toBe(300);
+describe("Ten strikes game scores 300 points",
+    function() {
+        it("Should return 300 as the score for X|X|X|X|X|X|X|X|X|X||XX game",
+            function() {
+                var score = bowling.calculateScore("X|X|X|X|X|X|X|X|X|X||XX");
+                expect(score).toBe(300);
+            });
     });
-});
+
+describe("Nine pins hit of all frames scores 90 points",
+    function () {
+        it("Should return 90 as the score for 9-|9-|9-|9-|9-|9-|9-|9-|9-|9-|| game",
+            function() {
+                var score = bowling.calculateScore("9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||");
+                expect(score).toBe(90);
+            });
+    });
