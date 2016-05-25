172129811a56f1b894b1973c5d42d99c916e8b3c
Halima KOUNDI <hkoundi@gmail.com>
2016-05-25 17:37:37 +0100

    Calculate score for normal hits - no strike no spare

--- Javascript/Bowling/bowling.js
+++ Javascript/Bowling/bowling.js
@@ -1,3 +1,24 @@
-exports.calculateScore = function () {
-    return 20;
+exports.calculateScore = function (game) {
+    var rolls = game.toRolls();
+    return rolls.sum();
+}
+
+String.prototype.toRolls = function () {
+    var obj = [];
+    this.split('|').forEach(function (frame) {
+        obj.push({
+            "firstRollScore": parseInt(frame[0]),
+            "secondRollScore": parseInt(frame[1])
+        });
+    });
+    return obj;
+}
+
+Array.prototype.sum = function () {
+    var total = 0;
+    this.forEach(function (element) {
+        console.log(element);
+        total += element.firstRollScore + element.secondRollScore;
+    });
+    return total;
 }
--- Javascript/Bowling/spec/bowling-spec.js
+++ Javascript/Bowling/spec/bowling-spec.js
@@ -5,7 +5,17 @@ describe("Games containing normal scores",
 
         it("Should return 20 points for 10 ball rolls of one pin hit game",
             function() {
-                var score = bowling.calculateScore("11|11|11|11|11|11|11|11|11|11|");
+                var score = bowling.calculateScore("11|11|11|11|11|11|11|11|11|11");
+
                 expect(score).toBe(20);
             });
+
+        it("Should return 28 points for a game like '11|15|15|11|11|11|11|11|11|11' ",
+            function() {
+                var score = bowling.calculateScore("11|15|15|11|11|11|11|11|11|11");
+
+                expect(score).toBe(28);
+            });
+
     });
+
