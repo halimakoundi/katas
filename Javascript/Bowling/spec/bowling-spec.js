822a25bdd0eb52d69ac5d9674f2126dbc77b0578
Halima KOUNDI <hkoundi@gmail.com>
2016-05-27 09:57:47 +0100

    back to the beginning of times..

--- Javascript/Bowling/spec/bowling-spec.js
+++ Javascript/Bowling/spec/bowling-spec.js
@@ -1,73 +1,7 @@
 var bowling = require("../bowling.js");
 
-describe("Games containing normal scores",
+describe("",
     function () {
-
-        it("Should return 20 points for 10 ball rolls of one pin hit game",
-            function() {
-                var score = bowling.calculateScore("11|11|11|11|11|11|11|11|11|11");
-
-                expect(score).toBe(20);
-            });
-
-        it("Should return 28 points for a game like '11|15|15|11|11|11|11|11|11|11' ",
-            function() {
-                var score = bowling.calculateScore("11|15|15|11|11|11|11|11|11|11");
-
-                expect(score).toBe(28);
-            });
 
     });
 
-describe("Games containing miss hits",
-    function () {
-
-        it("Should score 27 for a game like '-1|15|15|11|11|11|11|11|11|11' ",
-            function() {
-                var score = bowling.calculateScore("-1|15|15|11|11|11|11|11|11|11");
-                expect(score).toBe(27);
-            }); 
-
-        it("Should score 24 for a game like '-1|15|15|11|-1|11|--|11|11|11' ",
-            function () {
-                var score = bowling.calculateScore("-1|15|15|11|-1|11|--|11|11|11");
-                expect(score).toBe(24);
-            });
-
-    });
-
-describe("Games containing spares",
-    function () {
-
-        it("Should score 37 for a game like '-/|15|15|11|11|11|11|11|11|11' ",
-            function () {
-                var score = bowling.calculateScore("-/|15|15|11|11|11|11|11|11|11");
-
-                expect(score).toBe(37);
-            });
-
-        it("Should score 46 for a game like '-/|15|15|1/|11|11|11|11|11|11' ",
-            function () {
-                var score = bowling.calculateScore("-/|15|15|1/|11|11|11|11|11|11");
-
-                expect(score).toBe(46);
-            });
-    });
-
-describe("Games containing strikes",
-    function () {
-
-        it("Should return 47 for a game like '-/|15|X|13|11|11|11|11|11|11' ",
-            function() {
-                var score = bowling.calculateScore("-/|15|X|13|11|11|11|11|11|11");
-
-                expect(score).toBe(47)
-            });
-
-        it("Should return 57 for a game like '-/|15|X|13|11|11|X|11|11|11' ",
-            function () {
-                var score = bowling.calculateScore("-/|15|X|13|11|11|X|11|11|11");
-
-                expect(score).toBe(57)
-            });
-    });
