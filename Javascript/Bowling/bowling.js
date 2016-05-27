b664b10ff6646672eff30d93cee5bcb3e4f4edd0
Halima KOUNDI <hkoundi@gmail.com>
2016-05-27 12:28:24 +0100

    Games with bonus rolls Should return a score of 13 for a spare on the last frame and 3 pin hit bonus roll

--- Javascript/Bowling/bowling.js
+++ Javascript/Bowling/bowling.js
@@ -1,32 +1,37 @@
+var frames = [];
+
 exports.calculateScore = function (game) {
     var score = 0;
     var mainGame = game.split("||")[0];
     var bonusRolls = game.split("||")[1];
-    var frames = mainGame.split("|");
+    frames = mainGame.split("|");
     frames.forEach(function (frame, currentFrameIndex) {
-        var firstRollScore = rollScore(frame, 0);
-        var secondRollScore = rollScore(frame, 1);
-
-        frameScore = firstRollScore + secondRollScore;
-        if (frameScore == 10) {
-            var nextFrameFirstRollScore = 0;
-            var isLastFrame = currentFrameIndex == (frames.length - 1);
-            if (isLastFrame) {
-                nextFrameFirstRollScore = rollScore(bonusRolls, 0);
-            } else {
-                var nextFrame = frames[currentFrameIndex + 1];
-                nextFrameFirstRollScore = rollScore(nextFrame, 0);
-            }
-            frameScore += nextFrameFirstRollScore;
-            if (secondRollScore == 0) {
-                var nextFrameSecondRollScore = rollScore(nextFrame, 1);
-                frameScore += nextFrameSecondRollScore;
-            }
-        }
-        score += frameScore;
+        score += frameScore(frame, currentFrameIndex);
     });
     return score;
 };
+
+var frameScore = function (frame, currentFrameIndex) {
+    var firstRollScore = rollScore(frame, 0);
+    var secondRollScore = rollScore(frame, 1);
+
+    frameScore = firstRollScore + secondRollScore;
+    if (frameScore == 10) {
+        var nextFrameFirstRollScore = 0;
+        var isLastFrame = currentFrameIndex == (frames.length - 1);
+        if (isLastFrame) {
+            nextFrameFirstRollScore = rollScore(bonusRolls, 0);
+        } else {
+            var nextFrame = frames[currentFrameIndex + 1];
+            nextFrameFirstRollScore = rollScore(nextFrame, 0);
+        }
+        frameScore += nextFrameFirstRollScore;
+        if (secondRollScore == 0) {
+            var nextFrameSecondRollScore = rollScore(nextFrame, 1);
+            frameScore += nextFrameSecondRollScore;
+        }
+    }
+}
 
 var rollScore = function (frame, rollIndex) {
     rollResult = frame[rollIndex];
