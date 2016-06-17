4847646ed98081ca1aabb0984b10600cfc3cb0d4
Halima KOUNDI <hkoundi@gmail.com>
2016-06-17 16:26:24 +0100

    add method handles new lines instead of commas

--- Javascript/Calculator/calculator.js
+++ Javascript/Calculator/calculator.js
@@ -1,6 +1,7 @@
 exports.add = function (addendum) {
     var result = 0;
     if (addendum) {
+        addendum = addendum.replace("\n", ",");
         var numbers = addendum.split(",");
         for (number of numbers) {
             result += parseInt(number);
--- Javascript/Calculator/spec/calculator-spec.js
+++ Javascript/Calculator/spec/calculator-spec.js
@@ -3,6 +3,9 @@
  * Create a simple String calculator with a method int Add(string numbers)
  * The method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example “” or “1” or “1,2”
  * Allow the Add method to handle an unknown amount of numbers
+ * Allow the Add method to handle new lines between numbers (instead of commas).
+ * a. the following input is ok: “1\n2,3” (will equal 6)
+ * b. the following input is NOT ok: “1,\n” (not need to prove it - just clarifying)
  */
 var calculator = require("../calculator.js");
 
@@ -29,10 +32,12 @@ describe("String calculator ",
 
                 expect(sum).toBe(3);
             });
-        it("Should return the sum of the multiple numbers string parameter passed to it",
-            function () {
-                var sum = calculator.add("1,2,4");
 
-                expect(sum).toBe(7);
+        it("Should handle new lines between numbers instead of commas",
+            function () {
+                var sum = calculator.add("1\n2,3");
+
+                expect(sum).toBe(6);
             });
+
     });
