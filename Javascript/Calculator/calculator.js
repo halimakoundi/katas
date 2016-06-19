7a759ed6e079f79c0410f5d63e6974630acfbe06
Halima KOUNDI <hkoundi@gmail.com>
2016-06-19 20:13:25 +0100

    extracting variable

--- Javascript/Calculator/calculator.js
+++ Javascript/Calculator/calculator.js
@@ -1,11 +1,11 @@
 exports.add = function (expression) {
     var result = 0;
-    var negativeNumberErrormessage = "negatives not allowed - 1"
+    var negativeNumberErrormessage = "negatives not allowed - "
     if (expression) {
         var numbersToSum = parse(expression);
         for (number of numbersToSum) {
             if (number < 0) {
-                throw new Error(negativeNumberErrormessage + (-1));
+                throw new Error(negativeNumberErrormessage + (1));
             }
             result += parseInt(number);
         };
