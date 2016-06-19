6c4efc322313d6c85769209a43b4c296244fb836
Halima KOUNDI <hkoundi@gmail.com>
2016-06-19 15:09:46 +0100

    refactoring

--- Javascript/Calculator/calculator.js
+++ Javascript/Calculator/calculator.js
@@ -1,8 +1,5 @@
 exports.add = function (expression) {
     var result = 0;
-    if (expression == ("//;\n1;2")) {
-        expression = expression.split("\n")[1];
-    }
     if (expression) {
         var numbers = parse(expression);
         for (number of numbers) {
@@ -11,10 +8,19 @@ exports.add = function (expression) {
     }
     return result;
 }
-
+var newLine = "\n";
 var defaultDelimiter = ",";
+
 var parse = function (expression) {
-    var delimiters = findDelimiters(expression);
+    var expressions = expression.split("\n");
+    var delimiters = [];
+    var delimiterDefiner = "//";
+    if (expressions.length > 1 && expression.indexOf(delimiterDefiner) > -1) {
+        delimiters.push(expressions[0].replace(delimiterDefiner, ""));
+        expression = expressions[1];
+    } else {
+        delimiters = findDelimiters(expression);
+    }
     expression = replaceDelimiters(expression, delimiters);
     var parameters = expression.split(defaultDelimiter);
     return parameters;
@@ -34,7 +40,6 @@ var replaceDelimiters = function (expression, delimiters) {
     for (delimiter of delimiters) {
         expression = expression.replace(delimiter, defaultDelimiter);
     }
-    var newLine = "\n";
     expression = expression.replace(newLine, defaultDelimiter);
     return expression;
 }
