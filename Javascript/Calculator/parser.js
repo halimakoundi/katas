af46b986795cc989e0b8e612e102dcd0f2f39846
Halima KOUNDI <hkoundi@gmail.com>
2016-06-22 09:29:12 +0100

    extract method isDelimiterDefinedInExpression

--- Javascript/Calculator/parser.js
+++ Javascript/Calculator/parser.js
@@ -12,9 +12,7 @@ var delimiterDefinerRegex = /^.*\[(.*?)].*/g;
 var extractNumbersToSum = function (expression) {
     var expressions = expression.split(newLine);
     var delimiters = [];
-    var isDelimiterDefinedInExpression = expressions.length > 1
-                                         && expression.indexOf(delimiterDefiner) > -1;
-    if (isDelimiterDefinedInExpression) {
+    if (isDelimiterDefinedInExpression(expressions)) {
         setDelimiters(expressions[0], delimiters);
         expression = expressions[1].replace(specialDelimiter, '');
     }
@@ -23,17 +21,23 @@ var extractNumbersToSum = function (expression) {
     expression = replaceDelimiters(expression, delimiters);
     var parameters = expression.split(defaultDelimiter)
                                .filter(function (n) { return n != '' })
-                               .map(item => { return parseInt(item);});;
+                               .map(item => { return parseInt(item); });;
     return parameters;
 }
 var setDelimiters = function (delimitingExpression, delimiters) {
     if (delimitingExpression.search(delimiterDefinerRegex) > -1) {
         var definedDelimiters = delimitingExpression.split("]");
         for (definedDelimiter of definedDelimiters) {
-            delimiters.push(definedDelimiter.replace("[",""));
+            delimiters.push(definedDelimiter.replace("[", ""));
         }
     }
 }
+
+var isDelimiterDefinedInExpression = function (expressions) {
+    return expressions.length > 1
+            && expressions[0].indexOf(delimiterDefiner) > -1;
+}
+
 
 var findDelimiters = function (expression, delimiters) {
     for (element of expression) {
