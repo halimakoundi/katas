4653d3ec539442f6bdae978d5499a2df7d80593f
Halima KOUNDI <hkoundi@gmail.com>
2016-06-23 17:10:57 +0100

    move validation of cell to board class

--- TicTacToeKata/TicTacToeKata.Src/TicTacToe.cs
+++ TicTacToeKata/TicTacToeKata.Src/TicTacToe.cs
@@ -1,26 +1,13 @@
-﻿using System;
-
-namespace TicTacToeKata.Src
+﻿namespace TicTacToeKata.Src
 {
     public class TicTacToe
     {
-        private Cell _cell;
         private GameResult _result = GameResult.IN_PROGRESS;
 
         private readonly Board _board = new Board();
 
-        private void ValidateCell(Cell cell)
-        {
-            if (_cell != null && _cell.Equals(cell))
-            {
-                throw new Exception("The cell is not empty");
-            }
-            _cell = cell;
-        }
-
         public void Play(Cell cell)
         {
-            ValidateCell(cell);
             _board.AddCell(cell);
         }
 
