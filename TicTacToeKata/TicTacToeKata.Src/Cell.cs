3e067953d574fae42f47a31cdd1fc3812ad34f3c
Halima KOUNDI <hkoundi@gmail.com>
2016-06-23 15:12:20 +0100

    extracting class letter

--- TicTacToeKata/TicTacToeKata.Src/Cell.cs
+++ TicTacToeKata/TicTacToeKata.Src/Cell.cs
@@ -2,29 +2,30 @@
 {
     public class Cell
     {
-        private readonly string _letter;
         private readonly Position _position;
+        private readonly Letter _letter;
 
         public Cell(string letter, int column, int row)
         {
-            _letter = letter;
+            _letter = new Letter(letter);
             _position = new Position(column, row);
         }
 
         public Cell()
         {
             _position = new Position();
+            _letter = new Letter();
         }
 
         public bool IsEmpty()
         {
-            return string.IsNullOrEmpty(_letter);
+            return _letter.IsEmpty();
         }
 
         public override bool Equals(object obj)
         {
             var otherCell = (Cell)obj;
-            return _letter == otherCell._letter
+            return _letter.Equals(otherCell._letter)
                    && _position.Equals(otherCell._position);
         }
     }
