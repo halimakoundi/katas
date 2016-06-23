c70bf837e18e77a9da2b53b40e78ec0db1f077d7
Halima KOUNDI <hkoundi@gmail.com>
2016-06-23 10:02:30 +0100

    game should not add letter on non empty position

--- TicTacToeKata/TicTacToeKata.Src/TicTacToe.cs
+++ TicTacToeKata/TicTacToeKata.Src/TicTacToe.cs
@@ -2,8 +2,19 @@
 {
     public class TicTacToe
     {
+        private string letter;
+        private int column;
+        private int row;
+
         public bool Play(string letter, int column, int row)
         {
+            if (letter == this.letter && column == this.column && row == this.row)
+            {
+                return false;
+            }
+            this.letter = letter;
+            this.row = row;
+            this.column = column;
             return true;
         }
 
--- TicTacToeKata/TicTacToeKata.Tests/TicTacToeShould.cs
+++ TicTacToeKata/TicTacToeKata.Tests/TicTacToeShould.cs
@@ -19,11 +19,25 @@ namespace TicTacToeKata.Tests
     [TestFixture]
     public class TicTacToeShould
     {
-        [TestCase("X",1,3, true)]
+        [TestCase("X", 1, 3, true)]
         public void add_an_X_to_the_board_on_given_coordinates(string letter, int column, int row, bool expected)
         {
             var game = new TicTacToe();
             Assert.AreEqual(expected, game.Play(letter, column, row));
+        }
+
+        [Test]
+        public void not_add_a_letter_to_the_board_on_non_empty_positon()
+        {
+            var game = new TicTacToe();
+            var letter = "X";
+            var column = 1;
+            var row = 2;
+            var result = game.Play(letter, column, row);
+
+            result = game.Play(letter, column, row);
+
+            Assert.That(result, Is.EqualTo(false));
         }
 
     }
