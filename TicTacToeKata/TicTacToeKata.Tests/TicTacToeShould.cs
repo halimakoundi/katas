c143aa96a5c70f4962f08259bfd90944e9e89965
Halima KOUNDI <hkoundi@gmail.com>
2016-06-23 13:00:00 +0100

    extract fields game and turns

--- TicTacToeKata/TicTacToeKata.Tests/TicTacToeShould.cs
+++ TicTacToeKata/TicTacToeKata.Tests/TicTacToeShould.cs
@@ -21,23 +21,26 @@ namespace TicTacToeKata.Tests
     [TestFixture]
     public class TicTacToeShould
     {
+        private List<Cell> _turns;
+        private TicTacToe _game;
+
         [TestCase("X", 1, 3, true)]
         public void add_an_X_to_the_board_on_given_coordinates(string letter, int column, int row, bool expected)
         {
-            var game = new TicTacToe();
-            Assert.AreEqual(expected, game.Play(letter, column, row));
+            _game = new TicTacToe();
+            Assert.AreEqual(expected, _game.Play(letter, column, row));
         }
 
         [Test]
         public void not_add_a_letter_to_the_board_on_non_empty_positon()
         {
-            var game = new TicTacToe();
+            _game = new TicTacToe();
             var letter = "X";
             var column = 1;
             var row = 2;
-            var result = game.Play(letter, column, row);
+            var result = _game.Play(letter, column, row);
 
-            result = game.Play(letter, column, row);
+            result = _game.Play(letter, column, row);
 
             Assert.That(result, Is.EqualTo(false));
         }
@@ -45,16 +48,16 @@ namespace TicTacToeKata.Tests
         [Test]
         public void add_an_O_to_the_board_after_an_X_is_added_at_a_different_position()
         {
-            var game = new TicTacToe();
+            _game = new TicTacToe();
             var letter = "X";
             var column = 1;
             var row = 2;
-            game.Play(letter, column, row);
+            _game.Play(letter, column, row);
 
             letter = "O";
             column = 3;
             row = 1;
-            var result = game.Play(letter, column, row);
+            var result = _game.Play(letter, column, row);
 
             Assert.That(result, Is.EqualTo(true));
         }
@@ -62,8 +65,8 @@ namespace TicTacToeKata.Tests
         [Test]
         public void declare_game_as_draw_when_board_is_full_with_no_winner()
         {
-            var game = new TicTacToe();
-            var turns = new List<Cell>
+            _game = new TicTacToe();
+            _turns = new List<Cell>
             {
                 new Cell("X", 1, 1),
                 new Cell("O", 3, 3),
@@ -79,18 +82,18 @@ namespace TicTacToeKata.Tests
             var index = 0;
             while (index < 9)
             {
-                game.Play(turns[index]);
+                _game.Play(_turns[index]);
                 index += 1;
             }
 
-            Assert.That(game.GetResult(), Is.EqualTo(GameResult.DRAW));
+            Assert.That(_game.GetResult(), Is.EqualTo(GameResult.DRAW));
         }
 
         [Test]
         public void declare_X_as_winner_when_3_Xs_are_horizontally_adjacent()
         {
             var game = new TicTacToe();
-            var turns = new List<Cell>
+            _turns = new List<Cell>
             {
                 new Cell("X", 1, 1),
                 new Cell("O", 3, 3),
@@ -100,9 +103,9 @@ namespace TicTacToeKata.Tests
             };
 
             var index = 0;
-            while (index < turns.Count)
+            while (index < _turns.Count)
             {
-                game.Play(turns[index]);
+                game.Play(_turns[index]);
                 index += 1;
             }
 
