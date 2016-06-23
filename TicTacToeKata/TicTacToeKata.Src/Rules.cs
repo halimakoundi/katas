5cf3e98f867c246c4195cf83b1494091ceb32067
Halima KOUNDI <hkoundi@gmail.com>
2016-06-23 15:50:11 +0100

    extracting rules class

--- TicTacToeKata/TicTacToeKata.Src/Rules.cs
+++ TicTacToeKata/TicTacToeKata.Src/Rules.cs
@@ -5,12 +5,12 @@ namespace TicTacToeKata.Src
 {
     public class Rules
     {
-        private static readonly List<string> _validLetters = new List<string> { "X", "O" };
+        private static readonly List<string> ValidLetters = new List<string> { "X", "O" };
 
         public static void ValidateSubmittedLetter(string letter)
         {
             if (string.IsNullOrEmpty(letter)
-                || !_validLetters.Contains(letter))
+                || !ValidLetters.Contains(letter))
             {
                 throw new Exception($"Provided letter is not valid: {letter}");
             }
