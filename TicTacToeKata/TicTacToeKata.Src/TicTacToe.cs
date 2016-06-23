282a11bc599fcb30d2890724b0b033832720c48f
Halima KOUNDI <hkoundi@gmail.com>
2016-06-23 16:42:43 +0100

    introduce validate cell function

--- TicTacToeKata/TicTacToeKata.Src/TicTacToe.cs
+++ TicTacToeKata/TicTacToeKata.Src/TicTacToe.cs
@@ -8,12 +8,23 @@
 
         public bool Play(string letter, int column, int row)
         {
-            if (_cell != null && !_cell.Equals(new Cell(new Letter(letter), new Position(new Column(column), new Row(row)))))
+            if (ValidateCell(new Cell(new Letter(letter), new Position(new Column(column), new Row(row))))) return true;
+            return false;
+        }
+
+        private bool ValidateCell(Cell cell)
+        {
+            if (_cell != null && !_cell.Equals(cell))
             {
-                _cell = new Cell(new Letter(letter), new Position(new Column(column), new Row(row)));
+                _cell = cell;
                 return true;
             }
             return false;
+        }
+
+        public void Play(Cell cell)
+        {
+            this._board += 1;
         }
 
         public GameResult GetResult()
@@ -27,11 +38,6 @@
                 return GameResult.X_Wins;
             }
             return GameResult.DRAW;
-        }
-
-        public void Play(Cell cell)
-        {
-            this._board += 1;
         }
     }
 }
